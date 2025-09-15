efb64d2c27e6b9a4f67add1e74fb9178bcc91eb7
mydevhero <mydevhero@gmail.com>
2025-09-16 00:00:54 +0200

    Verify sanitize

--- valgrind.sh
+++ valgrind.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #valgrind  --suppressions=valgrind.supp --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose ./build/Desktop-Release/caos
 #valgrind  --suppressions=valgrind.supp --leak-check=full --verbose ./build/Desktop-Release/caos
-valgrind  --suppressions=valgrind.supp --leak-check=full --verbose ./build/Desktop-Debug/caos
+valgrind  --suppressions=valgrind.supp --leak-check=full --show-leak-kinds=all --verbose ./build/Desktop-Debug/caos
